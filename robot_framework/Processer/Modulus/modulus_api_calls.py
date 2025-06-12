931a8fb7890d0de1ad62ec8518144c6eadc3bde9
Michael Fredslund Larsen <lfrmi@aarhus.dk>
2025-06-12 10:30:31 +0200

    mere

--- robot_framework/Processer/Modulus/modulus_api_calls.py
+++ robot_framework/Processer/Modulus/modulus_api_calls.py
@@ -1,15 +1,19 @@
 """henter alle fil id'er på en borgers sag."""
 import json
 import os
+import re
 from datetime import datetime
 from urllib.parse import urlparse
+from urllib.parse import unquote
 import requests
 
 
 def get_fileids_by_case_api(cookie: str, caseid: str, startdato: str, slutdato: str) -> list:
     """Henter alle fil id'er på en borgers sag, filtreret efter journaliseringsdato."""
 
-    url = f"https://aarhus.modulussocial.dk/odata/SflDocument/Default.GetByCase(Id={caseid})?$select=title,fileSize,fileType,finalized,journalized,remarkRegardingSubjectAccess,id,isSensitive,type&$orderby=journalized%20desc&$skip=0&$top=20&$count=true"
+    url = f"https://aarhus.modulussocial.dk/odata/SflDocument/Default.GetByCase(Id={caseid})?$select=title,description,fileSize,fileType,finalized,journalized,remarkRegardingSubjectAccess,id,isSensitive,type&$orderby=journalized%20desc&$skip=0&$top=20&$count=true"
+
+    # url = f"https://aarhus.modulussocial.dk/odata/Activity/Default.GetByCase(Id={caseid})?$select=note,type,status,subtype,mandatory,completed,eventDate,description,hasDocuments,subjectAccess,remarkRegardingSubjectAccess,id,lastUpdatedCorrelationId,isSensitive&$orderby=eventDate%20desc&$skip=0&$top=20&$count=true"
 
     payload = {}
     headers = {
@@ -51,22 +55,23 @@ def get_fileids_by_case_api(cookie: str, caseid: str, startdato: str, slutdato: 
     return id_list
 
 
-def get_filelinks_by_fileid_api(cookie: str, fileid: int, serial, cpr):
+def get_filelinks_by_fileid_api(cookie: str, fileid: int, serial, cpr, sag):
     """henter alle fil id'er på en borgers sag."""
 
     url = f"https://aarhus.modulussocial.dk/api/activityDocuments/edit/{fileid}"
 
     payload = {}
     headers = {
-    'Accept': 'application/json, text/plain, */*',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'Accept-Language': 'da-DK,da;q=0.9,en-US;q=0.8,en;q=0.7',
     'Connection': 'keep-alive',
-    'Referer': 'https://aarhus.modulussocial.dk/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
-    'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'Cookie': cookie
@@ -98,17 +103,30 @@ def get_filelinks_by_fileid_api(cookie: str, fileid: int, serial, cpr):
 
     # Parse the URL to extract the document name and extension
     parsed_url = urlparse(file_link)
-    file_path = parsed_url.path
+    file_path = unquote(parsed_url.path)  # Decode the URL-encoded path
     document_name_with_extension = os.path.basename(file_path)
+    # Replace invalid characters in the filename
+    document_name_with_extension = re.sub(r'[<>:"/\\|?*]', '-', document_name_with_extension)
+
+    document_name, document_extension = os.path.splitext(document_name_with_extension)
 
     # Define the directory path
-    directory_path = rf"\\srvsql46\INDBAKKE\AAK_Aktindsigt\{serial}_{cpr}\Modulus"
+    directory_path = rf"\\srvsql46\INDBAKKE\AAK_Aktindsigt\{serial}_{cpr}\Modulus\{sag}"
 
     # Create the directory if it does not exist
     os.makedirs(directory_path, exist_ok=True)
 
-    # Save the file content to a local file
+    # Normalize the directory path
+    directory_path = os.path.normpath(directory_path)
+
+    counter = 1
     file_save_path = os.path.join(directory_path, document_name_with_extension)
+
+    # Check if the file already exists and modify the filename if necessary
+    while os.path.exists(file_save_path):
+        file_save_path = os.path.join(directory_path, f"{document_name}_{counter}{document_extension}")
+        counter += 1
+
     with open(file_save_path, 'wb') as file:
         file.write(file_response.content)
 
--- robot_framework/maintest.py
+++ robot_framework/maintest.py
@@ -19,15 +19,17 @@ gemt_credential = orchestrator_connection.get_credential("032_Boerns_Trivsel")
 bearer_token = use_retry_logic(modulus_login, username=gemt_credential.username, password=gemt_credential.password, drivertype='chrome_wire')
 # login
 
-test_dict = {"Bestiller":"lfrmi@aarhus.dk","CPR":"0909108999","Navn":"Per Familie","Startdato":"2025-01-01","Slutdato":"2025-06-30","SagsID":"114654","Serial":17}
+# test_dict = {"Bestiller":"lfrmi@aarhus.dk","CPR":"0909108999","Navn":"Per Familie","Startdato":"2024-01-01","Slutdato":"2025-06-30","SagsID":"114654","Serial":17}
+# 92400
+test_dict = {"Bestiller":"lfrmi@aarhus.dk","CPR":"AID4007012","Navn":"Astrid Test","Startdato":"2023-01-01","Slutdato":"2025-06-30","SagsID":"92388","Serial":17}
 
 # 132018 Astrid Testborger sag
-fileid_list = get_fileids_by_case_api(bearer_token, test_dict["SagsID"], test_dict["Startdato"], test_dict["Slutdato"])
+fileid_list = get_fileids_by_case_api(bearer_token, sag, test_dict["Startdato"], test_dict["Slutdato"])
 for fileid in fileid_list:
-    file_save_path = get_filelinks_by_fileid_api(bearer_token, fileid, test_dict["Serial"], test_dict["CPR"])
-    if file_save_path.endswith(".docx"):
-        docx_to_pdf(file_save_path)
-    else:
-        print("No word doc")
+    file_save_path = get_filelinks_by_fileid_api(bearer_token, fileid, test_dict["Serial"], test_dict["CPR"], sag)
+    # if file_save_path.endswith(".docx"):
+    #     docx_to_pdf(file_save_path)
+    # else:
+    #     print("No word doc")
 
 print("Stop her")
